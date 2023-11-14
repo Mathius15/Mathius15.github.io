e2fc0202f6c65d2086a476803f21e4fd3510c715
Matias Moraga <matimoraga10@gmail.com>
2023-11-14 19:07:59 -0300

    Arreglo posiciones de todo y agrego el efecto al duende verde

--- TP4/script.js
+++ TP4/script.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var parallaxLayers = document.querySelectorAll('.parallax-layer');
-  
-    if (parallaxLayers.length > 0) {
-      window.addEventListener('scroll', function() {
-        parallaxLayers.forEach(function(layer) {
-          var speed = parseFloat(layer.getAttribute('data-speed'));
-          var yPos = -(window.scrollY * speed / 100);
-          layer.style.transform = 'translate3d(0px, ' + yPos + 'px, 0px)';
-        });
-      });
+const observer = new IntersectionObserver((entries) => {//se ejecuta cada vez que la visibilidad de un objeto cambia
+    entries.forEach((entry) => {//usa foreach porque pueden ser varios elementos
+        console.log(entry)
+        if (entry.isIntersecting) {//si ese objeto es visible le cambia la clase a show
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden'); //agarro todos los elementos con la clase hidden
+hiddenElements.forEach((el) => observer.observe(el));//observa todos los elementos hiddenElements
+
+
+const parallaxDuende = document.getElementById('img-duende'); //agarro la imagen del duende verde
+let initialTop = 130; //es de donde arranca a moverse la imagen
+window.addEventListener('scroll', function () {
+    let offset = window.scrollY;//toma la pos Y de donde esta el usuario
+    let newPosition = initialTop + offset * 0.2; //hace que la pos sea menos a la del scroll
+    if(newPosition <250){
+        parallaxDuende.style.top = newPosition + 'px'; //cambia la pos dependiendo del scroll
     }
-  });
-  
+})

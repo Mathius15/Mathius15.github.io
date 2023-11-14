2c46e29498c6059fc8b421d23c44412508d00dbe
Matias Moraga <matimoraga10@gmail.com>
2023-11-14 19:40:47 -0300

    agrego animacion de aparicion a spiders, edificio y telarañas

--- TP4/script.js
+++ TP4/script.js
@@ -1,17 +1,45 @@
-const observer = new IntersectionObserver((entries) => {//se ejecuta cada vez que la visibilidad de un objeto cambia
+const observerEdificios = new IntersectionObserver((entries) => {//se ejecuta cada vez que la visibilidad de un objeto cambia
     entries.forEach((entry) => {//usa foreach porque pueden ser varios elementos
         console.log(entry)
         if (entry.isIntersecting) {//si ese objeto es visible le cambia la clase a show
-            entry.target.classList.add('show');
+            entry.target.classList.add('showEdificios');
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('showEdificios');
+        }
+    });
+});
+
+const observerArañas = new IntersectionObserver((entries) => {//se ejecuta cada vez que la visibilidad de un objeto cambia
+    entries.forEach((entry) => {//usa foreach porque pueden ser varios elementos
+        console.log(entry)
+        if (entry.isIntersecting) {//si ese objeto es visible le cambia la clase a show
+            entry.target.classList.add('showArañas');
+        } else {
+            entry.target.classList.remove('showArañas');
+        }
+    });
+});
+
+const observerTelaraña = new IntersectionObserver((entries) => {//se ejecuta cada vez que la visibilidad de un objeto cambia
+    entries.forEach((entry) => {//usa foreach porque pueden ser varios elementos
+        console.log(entry)
+        if (entry.isIntersecting) {//si ese objeto es visible le cambia la clase a show
+            entry.target.classList.add('showTelarañas');
+        } else {
+            entry.target.classList.remove('showTelarañas');
         }
     });
 });
 
 
-const hiddenElements = document.querySelectorAll('.hidden'); //agarro todos los elementos con la clase hidden
-hiddenElements.forEach((el) => observer.observe(el));//observa todos los elementos hiddenElements
+const hiddenEdificios = document.querySelectorAll('.hiddenEdificios'); //agarro todos los elementos con la clase hidden
+hiddenEdificios.forEach((el) => observerEdificios.observe(el));//observa todos los elementos hiddenElements
+
+const hiddenArañas = document.querySelectorAll('.hiddenArañas');
+hiddenArañas.forEach((el) => observerArañas.observe(el));
+
+const hiddenTelarañas = document.querySelectorAll('.hiddenTelarañas');
+hiddenTelarañas.forEach((el) => observerTelaraña.observe(el));
 
 
 const parallaxDuende = document.getElementById('img-duende'); //agarro la imagen del duende verde
